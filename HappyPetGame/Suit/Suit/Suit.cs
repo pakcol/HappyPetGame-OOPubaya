6e089f06b3daa155e9bc72bb39e5a0adeb709427
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 11:16:17 +0700

    Create Suit.cs

---
+++ HappyPetGame/Suit/Suit/Suit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Suit
+{
+    public class Suit
+    {
+        private string playerSuit;
+        private Image picture;
+
+        #region constructors
+        public Suit(string playerSuit, Image picture)
+        {
+            this.PlayerSuit = playerSuit;
+            this.Picture = picture;
+        }
+        #endregion
+
+        #region properties
+        public string PlayerSuit { get => playerSuit; set => playerSuit = value; }
+        public Image Picture { get => picture; set => picture = value; }
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
