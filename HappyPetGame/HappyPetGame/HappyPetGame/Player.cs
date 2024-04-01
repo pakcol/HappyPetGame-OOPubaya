a6c1beb8d92eade45eb2292affb4dc495efc1c19
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 11:05:29 +0700

    Create Player.cs

---
+++ HappyPetGame/HappyPetGame/HappyPetGame/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HappyPetGame
+{
+    [Serializable]
+
+    public class Player
+    {
+        private string name;
+        private int coins;
+        private DateTime lastPlay;
+        #region Constructor
+        public Player(string name, int coins, DateTime lastPlay)
+        {
+            this.Name = name;
+            this.Coins = coins;
+            this.LastPlay = lastPlay;
+        }
+        #endregion
+        #region Properties
+        public string Name 
+        { 
+            get => name; 
+            set
+            {
+                if(value != "")
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new Exception("Player name cannot be empty");
+                }
+            }
+        }
+        public int Coins 
+        { 
+            get => coins; 
+            set
+            {
+                if(value >= 0)
+                {
+                    coins = value;
+                }
+                else
+                {
+                    coins = 0;
+                }
+            }
+        }
+        public DateTime LastPlay { get => lastPlay; set => lastPlay = value; }
+        #endregion
+        #region Method
+        //semua child harus punya method DisplayData
+        public string DisplayData()
+        {
+            string data = this.Name + 
+                          "\nCoins : " + this.Coins 
+                          ;
+            return data;
+        }
+
+        #endregion
+    }
+}
