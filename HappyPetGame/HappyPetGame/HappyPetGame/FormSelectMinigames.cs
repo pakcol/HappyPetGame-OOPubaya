f9c83e7cd4910e3b6a69cdce8a18d8bf53808db8
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 11:03:01 +0700

    Create FormSelectMinigames.cs

---
+++ HappyPetGame/HappyPetGame/HappyPetGame/FormSelectMinigames.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HappyPetGame
+{
+    public partial class FormSelectMinigames : Form
+    {
+        FormGame frmGame;
+        public FormSelectMinigames()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxSnake_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSuit_Click(object sender, EventArgs e)
+        {
+            FormChooseSuit frmSuit = new FormChooseSuit();
+            frmSuit.Owner = this;
+            frmSuit.ShowDialog();
+        }
+
+        private void FormSelectMinigames_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            frmGame.timerGame.Enabled = true;
+        }
+
+        private void FormSelectMinigames_Load(object sender, EventArgs e)
+        {
+            frmGame = (FormGame)this.Owner;
+        }
+
+        private void buttonSnake_Click(object sender, EventArgs e)
+        {
+            FormSnakeGame frmSnake = new FormSnakeGame();
+            frmSnake.Owner = this;
+            frmSnake.ShowDialog();
+        }
+    }
+}
