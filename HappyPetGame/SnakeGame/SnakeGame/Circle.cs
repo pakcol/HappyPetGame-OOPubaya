a8da9c5b7417080657046ce5727160db1b616aeb
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 11:13:19 +0700

    Create Circle.cs

---
+++ HappyPetGame/SnakeGame/SnakeGame/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeGame
+{
+    public class Circle
+    {
+        #region Data Members
+        private int x;
+        private int y;
+        #endregion
+
+        #region Constructors
+        public Circle()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+        public Circle(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        #endregion
+
+        #region Properties
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        #endregion
+    }
+}
