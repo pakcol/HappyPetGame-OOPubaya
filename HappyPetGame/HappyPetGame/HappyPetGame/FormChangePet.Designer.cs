50eb595b904644a5743186d44e8889f198f08491
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 10:55:05 +0700

    Create FormChangePet.Designer.cs

---
+++ HappyPetGame/HappyPetGame/HappyPetGame/FormChangePet.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace HappyPetGame
+{
+    partial class FormChangePet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonChameleon = new System.Windows.Forms.RadioButton();
+            this.radioButtonFish = new System.Windows.Forms.RadioButton();
+            this.radioButtonCat = new System.Windows.Forms.RadioButton();
+            this.buttonLetsPlay = new System.Windows.Forms.Button();
+            this.comboBoxEnvironment = new System.Windows.Forms.ComboBox();
+            this.textBoxPetName = new System.Windows.Forms.TextBox();
+            this.labelEnvironment = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButtonChameleon
+            // 
+            this.radioButtonChameleon.BackgroundImage = global::HappyPetGame.Properties.Resources.chameleon_happy;
+            this.radioButtonChameleon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButtonChameleon.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButtonChameleon.Font = new System.Drawing.Font("Sitka Display", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonChameleon.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.radioButtonChameleon.Location = new System.Drawing.Point(390, 11);
+            this.radioButtonChameleon.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButtonChameleon.Name = "radioButtonChameleon";
+            this.radioButtonChameleon.Size = new System.Drawing.Size(184, 161);
+            this.radioButtonChameleon.TabIndex = 30;
+            this.radioButtonChameleon.TabStop = true;
+            this.radioButtonChameleon.Text = "Chameleon";
+            this.radioButtonChameleon.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.radioButtonChameleon.UseVisualStyleBackColor = true;
+            this.radioButtonChameleon.CheckedChanged += new System.EventHandler(this.radioButtonChameleon_CheckedChanged);
+            // 
+            // radioButtonFish
+            // 
+            this.radioButtonFish.BackgroundImage = global::HappyPetGame.Properties.Resources.fish_happy;
+            this.radioButtonFish.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButtonFish.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButtonFish.Font = new System.Drawing.Font("Sitka Display", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFish.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.radioButtonFish.Location = new System.Drawing.Point(197, 11);
+            this.radioButtonFish.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButtonFish.Name = "radioButtonFish";
+            this.radioButtonFish.Size = new System.Drawing.Size(185, 161);
+            this.radioButtonFish.TabIndex = 29;
+            this.radioButtonFish.TabStop = true;
+            this.radioButtonFish.Text = "Fish";
+            this.radioButtonFish.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.radioButtonFish.UseVisualStyleBackColor = true;
+            this.radioButtonFish.CheckedChanged += new System.EventHandler(this.radioButtonFish_CheckedChanged);
+            // 
+            // radioButtonCat
+            // 
+            this.radioButtonCat.BackgroundImage = global::HappyPetGame.Properties.Resources.cat_happy;
+            this.radioButtonCat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButtonCat.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButtonCat.Font = new System.Drawing.Font("Sitka Display", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonCat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.radioButtonCat.Location = new System.Drawing.Point(11, 11);
+            this.radioButtonCat.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButtonCat.Name = "radioButtonCat";
+            this.radioButtonCat.Size = new System.Drawing.Size(176, 161);
+            this.radioButtonCat.TabIndex = 28;
+            this.radioButtonCat.TabStop = true;
+            this.radioButtonCat.Text = "Cat";
+            this.radioButtonCat.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.radioButtonCat.UseVisualStyleBackColor = true;
+            this.radioButtonCat.CheckedChanged += new System.EventHandler(this.radioButtonCat_CheckedChanged);
+            // 
+            // buttonLetsPlay
+            // 
+            this.buttonLetsPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.buttonLetsPlay.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLetsPlay.Location = new System.Drawing.Point(203, 265);
+            this.buttonLetsPlay.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonLetsPlay.Name = "buttonLetsPlay";
+            this.buttonLetsPlay.Size = new System.Drawing.Size(179, 31);
+            this.buttonLetsPlay.TabIndex = 27;
+            this.buttonLetsPlay.Text = "Lets Play";
+            this.buttonLetsPlay.UseVisualStyleBackColor = false;
+            this.buttonLetsPlay.Click += new System.EventHandler(this.buttonLetsPlay_Click);
+            // 
+            // comboBoxEnvironment
+            // 
+            this.comboBoxEnvironment.FormattingEnabled = true;
+            this.comboBoxEnvironment.Location = new System.Drawing.Point(469, 196);
+            this.comboBoxEnvironment.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxEnvironment.Name = "comboBoxEnvironment";
+            this.comboBoxEnvironment.Size = new System.Drawing.Size(92, 21);
+            this.comboBoxEnvironment.TabIndex = 26;
+            // 
+            // textBoxPetName
+            // 
+            this.textBoxPetName.Location = new System.Drawing.Point(203, 196);
+            this.textBoxPetName.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPetName.Name = "textBoxPetName";
+            this.textBoxPetName.Size = new System.Drawing.Size(130, 20);
+            this.textBoxPetName.TabIndex = 25;
+            // 
+            // labelEnvironment
+            // 
+            this.labelEnvironment.AutoSize = true;
+            this.labelEnvironment.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEnvironment.Location = new System.Drawing.Point(338, 196);
+            this.labelEnvironment.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelEnvironment.Name = "labelEnvironment";
+            this.labelEnvironment.Size = new System.Drawing.Size(127, 19);
+            this.labelEnvironment.TabIndex = 24;
+            this.labelEnvironment.Text = "Environment :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 196);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 19);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Enter Your Pet Name :";
+            // 
+            // FormChangePet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(597, 304);
+            this.Controls.Add(this.radioButtonChameleon);
+            this.Controls.Add(this.radioButtonFish);
+            this.Controls.Add(this.radioButtonCat);
+            this.Controls.Add(this.buttonLetsPlay);
+            this.Controls.Add(this.comboBoxEnvironment);
+            this.Controls.Add(this.textBoxPetName);
+            this.Controls.Add(this.labelEnvironment);
+            this.Controls.Add(this.label2);
+            this.Name = "FormChangePet";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormChangePet";
+            this.Load += new System.EventHandler(this.FormChangePet_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonChameleon;
+        private System.Windows.Forms.RadioButton radioButtonFish;
+        private System.Windows.Forms.RadioButton radioButtonCat;
+        private System.Windows.Forms.Button buttonLetsPlay;
+        private System.Windows.Forms.ComboBox comboBoxEnvironment;
+        private System.Windows.Forms.TextBox textBoxPetName;
+        private System.Windows.Forms.Label labelEnvironment;
+        private System.Windows.Forms.Label label2;
+    }
+}
