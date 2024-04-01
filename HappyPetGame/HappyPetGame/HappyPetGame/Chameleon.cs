df4438992e575f6f7eaa987a48edfdead80c58b4
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 10:53:30 +0700

    Create Chameleon.cs

---
+++ HappyPetGame/HappyPetGame/HappyPetGame/Chameleon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace HappyPetGame
+{
+    [Serializable]
+
+    public class Chameleon : Pet
+    {
+        private Color currentColor;
+        #region Constructor
+        public Chameleon(string name, Image picture, Player owner, Color currentColor):base(name,picture,owner)
+        {
+            this.CurrentColor = currentColor;
+        }
+        #endregion
+        #region Properties
+        public Color CurrentColor { get => currentColor; set => currentColor = value; }
+        #endregion
+
+        #region Methods
+        public override string DisplayData()
+        {
+            string data = base.DisplayPet() +
+                        "\nColor : " + this.CurrentColor;
+            return data;
+        }
+
+        public override void Feed()
+        {
+            base.Health += 30;
+            base.Energy += 50;
+        }
+
+        public override void Sleep()
+        {
+            base.Health += 60;
+            base.Health += 60;
+        }
+
+        public void ChangeColor(Color newColor)
+        {
+            this.CurrentColor = newColor;
+        }
+        #endregion
+    }
+}
