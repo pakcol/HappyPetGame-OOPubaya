675ad68b8f8ca511d7757d27bb2f39cd6a139368
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 11:14:39 +0700

    Create ChooseSuit.cs

---
+++ HappyPetGame/Suit/Suit/ChooseSuit.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suit
+{
+    public partial class ChooseSuit : Form
+    {
+        BattleMenu battleMenu;
+        public string playerSuit = "";
+        public ChooseSuit()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLetsPlay_Click(object sender, EventArgs e)
+        {
+            if (radioButtonBatu.Checked)
+            {
+                playerSuit = "Batu";
+            }
+            else if (radioButtonGunting.Checked)
+            {
+                playerSuit = "Gunting";
+            }
+            else if (radioButtonKertas.Checked)
+            {
+                playerSuit = "Kertas";
+            }
+            BattleMenu form = new BattleMenu();
+            form.Owner = this;
+            form.ShowDialog();
+        }
+
+        private void ChooseSuit_Load(object sender, EventArgs e)
+        {
+            battleMenu = (BattleMenu)this.Owner;
+        }
+    }
+}
