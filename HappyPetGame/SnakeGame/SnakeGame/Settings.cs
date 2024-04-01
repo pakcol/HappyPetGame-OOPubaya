2437153e1e1966ffcfc46e24e14e307afce3286d
Stivent Nathaniel Riwu Laky <stivitsme@gmail.com>
2024-04-01 11:14:00 +0700

    Create Settings.cs

---
+++ HappyPetGame/SnakeGame/SnakeGame/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnakeGame
+{
+    public class Settings
+    {
+        //static karena tidak bisa diubah
+        private static int width;
+        private static int height;
+        private static string directions;
+
+        public static int Width { get => width; set => width = value; }
+        public static int Height { get => height; set => height = value; }
+        public static string Directions { get => directions; set => directions = value; }
+
+        public Settings() 
+        { 
+            Width = 16;
+            Height = 16;
+            Directions = "Left";
+        }
+    }
+}
